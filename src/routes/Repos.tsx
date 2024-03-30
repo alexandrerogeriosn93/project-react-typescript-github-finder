8183922c7e6fcbc8af5db78d3fb092f98eea465b
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-03-30 00:52:08 -0300

    feat: add function to get repositories by user name

--- src/routes/Repos.tsx
+++ src/routes/Repos.tsx
@@ -3,13 +3,37 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import BackBtn from "../components/BackBtn";
+import Loader from "../components/Loader";
+
+import { RepoProps } from "../types/repo";
 
 import classes from "./Repos.module.css";
 
 const Repos = () => {
   const { username } = useParams();
+  const [repos, setRepos] = useState<RepoProps[] | [] | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  return <div>Repos: {username}</div>;
+  useEffect(() => {
+    const loadRepos = async function (username: string) {
+      setIsLoading(true);
+
+      const res = await fetch(`http://api.github.com/users/${username}/repos`);
+      const data = await res.json();
+
+      setIsLoading(false);
+      console.log(data);
+    };
+
+    loadRepos(username);
+  }, []);
+
+  return (
+    <div>
+      <BackBtn />
+      Repos: {username}
+    </div>
+  );
 };
 
 export default Repos;
