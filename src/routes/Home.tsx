8c95477be09d9f8fd02c28e92bcfcd226594aced
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-03-29 23:17:50 -0300

    feat: create Error component

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+import Error from "../components/Error";
 import Search from "../components/Search";
 import User from "../components/User";
 
@@ -7,10 +8,20 @@ import { UserProps } from "../types/user";
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
 
   const loadUser = async (userName: string) => {
+    setError(false);
+    setUser(null);
+
     const res = await fetch(`https://api.github.com/users/${userName}`);
     const data = await res.json();
+
+    if (res.status === 404) {
+      setError(true);
+      return;
+    }
+
     const { avatar_url, login, location, followers, following } = data;
     const userData: UserProps = {
       avatar_url,
@@ -27,6 +38,7 @@ const Home = () => {
     <div>
       <Search loadUser={loadUser} />
       {user && <User {...user} />}
+      {error && <Error />}
     </div>
   );
 };
