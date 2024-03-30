34d1998e5ca02469e635110a9724f5b5e3e37a78
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-03-29 21:58:24 -0300

    feat: add router configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Outlet } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>GitHub finder</h1>
+      <Outlet />
     </div>
   );
 }
